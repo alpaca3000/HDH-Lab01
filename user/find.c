086ed9d6fe5b7a364f824a10a87febbe211237a8
Alpaca3000 <ruahuyenuyen@gmail.com>
2024-10-21 10:42:13 +0700

    add find.c

---
+++ user/find.c
@@ -0,0 +1,78 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/stat.h"
+
+// Helper function to combine 2 string
+char* strcat(char* dest, const char* src) {
+    char *os = dest;
+
+    while (*dest != '\0') {
+        dest++; 
+    }
+
+    while ((*dest++ = *src++) != 0) {
+    }
+
+    return os;
+}
+
+void find(char *path, char *filename)
+{
+    char buf[512];
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if ((fd = open(path, 0)) < 0) {
+        fprintf(2, "find: cannot open %s\n", path);
+        exit(1);
+    }
+
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        exit(1);
+    }
+
+    if (st.type == T_DIR) {
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) {  // fd thư mục hiện tại, &de thông tin thư mục hiện tại
+            if (de.inum == 0) {
+                continue;
+            }
+            if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) {
+                continue;
+            }
+
+            // Tạo đường dẫn bằng cách nối chuỗi
+            strcpy(buf, path);               
+            if (path[strlen(path) - 1] != '/') {  // thêm /
+                strcat(buf, "/");            
+            }
+            strcat(buf, de.name);            
+
+            if (stat(buf, &st) < 0) {
+                fprintf(2, "find: cannot stat %s\n", path);
+                continue;
+            }
+
+            if (st.type == T_DIR) {
+                find(buf, filename);  
+            } else if (strcmp(de.name, filename) == 0) {
+                printf("%s\n", buf);  // In đường dẫn nếu tên tệp khớp
+            }
+        }
+    }
+
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(2, "Usage: find <path> <filename>\n");
+        exit(1);
+    }
+
+    find(argv[1], argv[2]);
+    exit(0);
+}
